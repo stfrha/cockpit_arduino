57bd02db3fd65cac02b8daec6eec553d092c923b
Fredrik Hoffman <mail@fredrikhoffman.se>
2026-03-06 14:32:07 +0100

    Minor debugging and adding of buttons. Adding the ddi sketch, also built on the cockpit library.

--- ddi/ddi.ino
+++ ddi/ddi.ino
@@ -6,15 +6,79 @@
 // joystick button - one button on the usb interface, can be sourced from single or multiple device signals
 
 const int numOfDevices = 3;
-const int8_t deviceList[4] = {0, 1, 3, -1};
+const int8_t deviceList[4] = {0, 1, 2, -1};
 
 const int numOfSignalsPerDevice = 30;  // the number of signals in one device, according to the bitvector from the device
-const int numOfJoystickButtons = 59;   // The total number of joystick buttons that can be set
+const int numOfJoystickButtons = 69;   // The total number of joystick buttons that can be set
 
 uint8_t joystickButtonUpdates[numOfJoystickButtons];
 
 // Device index 0 (device 0)
 uint8_t signalToButtonTable[numOfDevices][numOfSignalsPerDevice] = {
+  {
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1
+  },
+  {
+    46,
+    47,
+    48,
+    49,
+    50,
+    51,
+    52,
+    53,
+    54,
+    55,
+    56,
+    57,
+    58,
+    59,
+    60,
+    61,
+    62,
+    63,
+    64,
+    65,
+    66,
+    67,
+    68,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1,
+    -1
+  },
   {
     0,
     1,
@@ -39,72 +103,8 @@ uint8_t signalToButtonTable[numOfDevices][numOfSignalsPerDevice] = {
     20,
     21,
     22,
-    23,
-    24,
-    25,
-    26,
-    27,
-    -1,
-    -1
-  },
-  {
     -1,
     -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    28,
-    29,
-    -1,
-    30,
-    31,
-    -1,
-    32,
-    33,
-    34,
-    35,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1
-  },
-  {
-    36,
-    37,
-    57,
-    58,
-    -1,
-    38,
-    39,
-    40,
-    -1,
-    -1,
-    42,
-    43,
-    45,
-    46,
-    -1,
-    47,
-    48,
-    49,
-    50,
-    51,
-    52,
-    53,
-    54,
-    55,
-    56,
     -1,
     -1,
     -1,
@@ -113,7 +113,7 @@ uint8_t signalToButtonTable[numOfDevices][numOfSignalsPerDevice] = {
   }
 };
 
-uint8_t rotaryEncoderJoystickButtons[4] = { 90, 91, 92, 93}; // Left CW, Left CCW, Right CW, Right CCW
+uint8_t rotaryEncoderJoystickButtons[4] = { 70, 71, 72, 73}; // Left CW, Left CCW, Right CW, Right CCW
 
 JoystickManager jMgr(
   numOfDevices,
@@ -125,54 +125,14 @@ JoystickManager jMgr(
   rotaryEncoderJoystickButtons);
 
 
-uint8_t button41State = 0; // 0 = 41 off, 1 = 41 pending, 2 = 41 On
-uint8_t button41PendingCounter = 0;
+uint8_t button39State = 0; // 0 = 39 off, 1 = 39 pending, 2 = 39 On
+uint8_t button39PendingCounter = 0;
 
 void handleSpecialJoystickButtonChanges(void)
 {
   // This is special handling for combinations of signals to joystick buttons
+  // ---of which there are none for the DDI
 
-  // RWR Rotary switch state machine
-  if (button41State == 0)
-  {
-    if ((joystickButtonUpdates[38] == 0) && (joystickButtonUpdates[39] == 0) && (joystickButtonUpdates[40] == 2))
-    {
-      button41State = 1;
-      button41PendingCounter = 0;
-    }
-  }
-  else if (button41State == 1)
-  {
-    if ((joystickButtonUpdates[38] == 0) && (joystickButtonUpdates[39] == 3) && (joystickButtonUpdates[40] == 0))
-    {
-      button41State = 0;
-    }
-    button41PendingCounter++;
-
-    if (button41PendingCounter >= 7)
-    {
-      button41State = 3;
-      joystickButtonUpdates[41] = 3;
-    }
-  }
-  else if (button41State == 3)
-  {
-    if ((joystickButtonUpdates[38] == 0) && (joystickButtonUpdates[39] == 0) && (joystickButtonUpdates[40] == 3))
-    {
-      button41State = 0;
-      joystickButtonUpdates[41] = 2;
-    }
-  }
-
-  // CMDS three way switch
-    if ((joystickButtonUpdates[42] == 2) || (joystickButtonUpdates[43] == 2))
-    {
-      joystickButtonUpdates[44] = 3;
-    }
-    else if ((joystickButtonUpdates[42] == 3) || (joystickButtonUpdates[43] == 3))
-    {
-      joystickButtonUpdates[44] = 2;
-    }
 }
 
 
